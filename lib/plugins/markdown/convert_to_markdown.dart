1fa5b1c123562a5b951d4ae3db9017083eb43115
CatHood0 <santiagowmar@gmail.com>
2025-03-01 18:22:49 -0400

    Removed future feature

--- lib/plugins/markdown/convert_to_markdown.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_quill_delta_easy_parser/flutter_quill_delta_easy_parser.dart';
-
-class MarkdownParser {
-  String parse(Document document) {
-    if(document.paragraphs.isEmpty) return '';
-    final String result = '';
-    return result; 
-  }
-}
